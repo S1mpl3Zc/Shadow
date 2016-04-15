da51a41553738123cec49a1d9cca93b70340571f
S1mpl3Zc <877386629@qq.com>
2016-04-15 11:53:59 +0800

    S1mpl3ZC
    ss

---
+++ Shadow/Shadow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shadow
+//
+//  Created by S1mpl3 on 16/4/15.
+//  Copyright © 2016年 S1mpl3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
